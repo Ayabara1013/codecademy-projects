fc5c09cecbc7875b2497798e0cf89f20d89ba933
Jeremy Allan <jallan8210@gmail.com>
2020-10-19 18:28:26 -0400

    new attempt, months later, get back on the horse!

---
+++ jammming/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './SearchResults.css';
+import TrackList from '../TrackList/TrackList.js';
+
+
+class SearchResults extends React.Component
+{
+  render()
+  {
+    return(
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/* <!-- Add a TrackList component --> */}
+        <TrackList tracks={this.props.searchResults} />
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
---
+++ jammming/src/Components/TrackList/TrackList.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react';
+import './TrackList.css';
+import Track from '../Track/Track.js';
+
+
+
+class TrackList extends React.Component
+{
+  render()
+  {
+    return(
+      <div className="TrackList">
+        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {
+          this.props.tracks.map(track => {
+            return <Track
+              track={track} 
+              key={track.id}
+            />
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default TrackList;
